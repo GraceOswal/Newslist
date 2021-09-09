25404182db51a1d11c23510413651bd334ec1858
graceoswal <graceoswal88@gmail.com>
2021-09-09 06:15:22 -0400

    --- Auto-Git Commit ---

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     """
     General configuration parent class
     """
-    NEWS_API_BASE_URL ='https://newsapi.org/v2/everything/news/{}?api-key={}'
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines/sources?apiKey={}'
 
 class ProdConfig(Config):
     '''
