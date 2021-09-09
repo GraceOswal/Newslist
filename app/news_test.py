528b1fbff62d6776735084da9a5b2f3d700f2e7c
graceoswal <graceoswal88@gmail.com>
2021-09-09 06:32:41 -0400

    add news class and initialize

--- app/news_test.py
+++ app/news_test.py
@@ -11,4 +11,11 @@ class Newstest(unittest.TestCase):
         '''
         This Set up method will run before every Test
         '''
-        self.new_news = News()
+        self.new_news = News('abc-news','ABC News','Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.',
+                            'https://abcnews.go.com', 'general', 'en-us')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+if __name__ == '__main__':
+    unittest.main()
