c1810c2243a3aa952678063ccc7a324c78e9b9d0
graceoswal <graceoswal88@gmail.com>
2021-09-09 06:50:39 -0400

    add unittest

--- app/models/news.py
+++ app/models/news.py
@@ -7,7 +7,7 @@ class News:
         self.id = id
         self.title = title
         self.description = description
-        self.poster = poster
+        self.poster = 'https://github.com/GraceOswal/Newslist/blob/master/abc%20image%201/img1.jpeg'/+poster
         self.url = url
         self.category = category
         self.language = language
--- app/news_test.py
+++ app/news_test.py
@@ -12,7 +12,7 @@ class Newstest(unittest.TestCase):
         This Set up method will run before every Test
         '''
         self.new_news = News('abc-news','ABC News','Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.',
-                            'https://abcnews.go.com', 'general', 'en-us')
+                            'https://github.com/GraceOswal/Newslist/blob/master/abc%20image%201/img1.jpeg', 'https://abcnews.go.com', 'general', 'en-us')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
