654b398d4e8ec683e45a669da456058496f002c1
graceoswal <graceoswal88@gmail.com>
2021-09-11 08:33:47 -0400

    --- Auto-Git Commit ---

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,5 @@ app.config.from_object(DevConfig)
 app.config.from_pyfile('instance/config.py')
 
 from app import views
+
+from app import error
--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 '''Model class for news app'''
 class News:
 
-    def __init__(self,source,title,description,urlimage,urltonews,category,language):
+    def __init__(self,source,title,description,urltoImage,urltonews,category,language):
         self.source = source
         self.title = title
         self.description = description
-        self.urltoimage = 'https://github.com/GraceOswal/Newslist/blob/master/abc%20image%201/img1.jpeg'
+        self.urltoImage = 'https://github.com/GraceOswal/Newslist/blob/master/abc%20image%201/img1.jpeg'
         self.urltonews = urltonews
         self.category = category
         self.language = language
