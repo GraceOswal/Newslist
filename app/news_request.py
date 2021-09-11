6f6c21811f4c12cf189d3b833341f41cc1a7d2dd
graceoswal <graceoswal88@gmail.com>
2021-09-11 05:24:37 -0400

    --- Auto-Git Commit ---

--- app/news_request.py
+++ app/news_request.py
@@ -1,37 +1,40 @@
-import requests
-
-from config import Config
+from app import app
+import urllib.request,json
+from .models import news
+from urllib import request
 
 class NewsRequest:
     def __init__(self):
 
-        self.API_KEY = Config.API_KEY
+        self.API_KEY = app.config['NEWS_API_KEY']
 
     def get_sources(self):
 
         sources = []
         sources_url = 'https://newsapi.org/v2/sources?q={}&apiKey={}'.format(sources, self.API_KEY)
-        response = requests.get(sources_url)
+        response = request.get(sources_url)
         if response.status_code == 200:
-            sources.append(data)
+            for data in response.json()['sources']:
+                sources.append(data)
         print(sources)
         return sources
 
     def get_articles(self, article):
-        article = []
-    article_url = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'.format(id, self.API_KEY)
-    response = requests.get(articles_url)
+        articles = []
+    articles_url = 'https://newsapi.org/v2/everything?q={}&apiKey={}'.format(articles, self.API_KEY)
+    response = request.get(articles_url)
     if response.status_code == 200:
-        for data in response.json()['articles']
-        articles.append(data)
-    print(articles)
-    return articles
+        for data in response.json()['articles']:
+            articles.append(data)
+        print(articles)
+        return articles
 
     def get_article_by_source(self, id):
         source_articles = []
-    source_articles_url = 'https://newsapi.org/v2/everything?sources={}?apiKey={}'.format(sources, self.API_KEY)
-    response = requests.get(source_articles_url)
+    source_articles_url = 'https://newsapi.org/v2/everything?sources={}?apiKey={}'.format(id, self.API_KEY)
+    response = request.get(source_articles_url)
     if response.status_code == 200:
-        source_articles.append(data)
-    print(source_article)
-    return source_article
+        for data in response.json()['articles']:
+            source_article.append(data)
+        print(source_article)
+        return source_article
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import 
 
 #Views
 @app.route('/news/<int:news_id>')
