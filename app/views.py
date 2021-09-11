959f58a5a08eae1063ef8ed650306161e663c33e
graceoswal <graceoswal88@gmail.com>
2021-09-11 06:05:07 -0400

    add news request

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app, news_request
 from .request import NewsRequest
+from urllib import request
 
 #Views
 @app.route('/')
@@ -16,4 +17,10 @@ def articles_page():
         articles = news_request.get_articles(search)
     else:
         articles = news_request.get_articles("verge")
-    return render_template('articles.html',)
+    return render_template('articles.html', articles=articles)
+
+@app.route('/article/<id>')
+def source_article(id):
+    source_articles = news_request.get_article_by_source(id)
+    source = id
+    return render_template('articles_display.html', source_articles=source_articles, source=source)
