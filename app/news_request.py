5764aa6edcc90e63079ec647837220533bc4a262
graceoswal <graceoswal88@gmail.com>
2021-09-11 04:53:23 -0400

    --- Auto-Git Commit ---

--- app/news_request.py
+++ app/news_request.py
@@ -12,4 +12,25 @@ class NewsRequest:
         sources = []
         sources_url = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'.format(sources, self.API_KEY)
         response = requests.get(sources_url)
-        if response.status_code == 
+        if response.status_code == 200:
+            sources.append(data)
+        print(sources)
+        return sources
+
+    def get_articles(self, article):
+        article = []
+    article_url = 'https://newsapi.org/v2/everything?q={}&apiKey={}'.format(article, self.API_KEY)
+    response = requests.get(articles_url)
+    if response.status_code == 200:
+        articles.append(data)
+    print(articles)
+    return articles
+
+    def get_article_by_source(self, id):
+        source = []
+    sources_url = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'.format(sources, self.API_KEY)
+    response = requests.get(sources_url)
+    if response.status_code == 200:
+        sources.append(data)
+    print(sources)
+    return sources
