32fdfba0cc0e1e8f792dc7ca1e59dcc3e491e4e1
graceoswal <graceoswal88@gmail.com>
2021-09-12 01:49:26 -0400

    update the models.py file

--- app/news_request.py
+++ app/news_request.py
@@ -1,12 +1,13 @@
-from app import app
-import urllib.request,json
-from .models import news
-from urllib import request
+import urllib.request
+
+from config import Config
+from app.main.views import source_article
+
 
 class NewsRequest:
     def __init__(self):
 
-        self.API_KEY = app.config['NEWS_API_KEY']
+        self.API_KEY = Config.API_KEY
 
     def get_sources(self):
 
@@ -21,21 +22,21 @@ class NewsRequest:
 
     def get_articles(self, article):
         articles = []
-    articles_url = 'https://newsapi.org/v2/everything?q={}&apiKey={}'.format(articles, self.API_KEY)
-    response = request.get(articles_url)
-    if response.status_code == 200:
-        for data in response.json()['articles']:
-            articles.append(data)
-        print(articles)
+        articles_url = 'https://newsapi.org/v2/everything?q={}&apiKey={}'.format(articles, self.API_KEY)
+        response = request.get(articles_url)
+        if response.status_code == 200:
+            for data in response.json()['articles']:
+                articles.append(data)
+            print(articles)
 
-        return articles
+            return articles
 
     def get_article_by_source(self, id):
         source_articles = []
-    source_articles_url = 'https://newsapi.org/v2/everything?sources={}?apiKey={}'.format(id, self.API_KEY)
-    response = request.get(source_articles_url)
-    if response.status_code == 200:
-        for data in response.json()['articles']:
-            source_article.append(data)
-        print(source_article)
-        return source_article
+        source_articles_url = 'https://newsapi.org/v2/everything?sources={}?apiKey={}'.format(id, self.API_KEY)
+        response = request.get(source_articles_url)
+        if response.status_code == 200:
+            for data in response.json()['articles']:
+                source_article.append(data)
+            print(source_article)
+            return source_article
