6fefe1aa327ec9919fbeb1a362896f6654007364
graceoswal <graceoswal88@gmail.com>
2021-09-12 07:48:01 -0400

    --- Auto-Git Commit ---

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,7 @@ from config import Config
 #Inializing application
 def create_app():
     app = Flask(__name__)
-
-#Blueprint configurations
     app.config.from_object(Config)
-
     from .main import main as main_blueprint
-
     app.register_blueprint(main_blueprint)
-
     return app
