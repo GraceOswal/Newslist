cad147c76e84376272ad66f9c8da5d0982945955
graceoswal <graceoswal88@gmail.com>
2021-09-12 01:01:09 -0400

    rename all files and folders

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 
 from flask import Flask
-from .config import DevConfig
+from .config import Config
 
 #Inializing application
 app = Flask(__name__, instance_relative_config = True)
 
 #This sets up configurations
-app.config.from_object(DevConfig)
+app.config.from_object(Config)
 
 app.config.from_pyfile('instance/config.py')
 
