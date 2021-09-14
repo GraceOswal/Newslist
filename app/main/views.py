8394c21058f6aeac1f64dd1a9925b7b8fbfa3d12
graceoswal <graceoswal88@gmail.com>
2021-09-14 11:01:09 -0400

    add all changes

--- app/main/views.py
+++ app/main/views.py
@@ -6,23 +6,20 @@ from ..import news_request as request
 from urllib.request import Request
 
 
-
-
 #Views
 @main.route('/')
 def index():
     sources = request.get_sources()
-    if sources:
-        return render_template('index.html', sources=sources)
+    
+    return render_template('index.html', sources=sources)
 
 @main.route('/articles', methods=["POST","GET"])
 def articles_page():
-    if request.method == 'POST':
+    if request == 'POST':
         search = request.form.get("search")
         articles = request.get_articles(search)
-    else:
-        articles = request.get_articles("verge")
-    return render_template('articles.html', articles=articles)
+
+    return render_template('articles.html')
 
 @main.route('/article/<id>')
 def source_article(id):
--- app/news_request.py
+++ app/news_request.py
@@ -12,16 +12,17 @@ base_url=None
 
 def configure_request(app):
     global api_key, base_url
-    api_key = app.config['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']
-    base_url = app.config ['NEWS_API_URL_ALL']
+    api_key ='2e763f47f2594cc791a0e476eaa77dcd'
+    base_url = app.config['NEWS_API_URL_ALL']
 
 
 def get_sources():
 
         sources = []
-        sources_url = 'https://newsapi.org/v2/sources?q={}&apiKey={}'.format(sources, api_key)
-        response = request.urlope(sources_url)
-        if response.status_code == 200:
+        sources_url = 'https://newsapi.org/v2/sources?q=tesla&apiKey={}'.format('d00a6496e549441e9d5edc3341b8a1bf')
+        response = request.urlopen(sources_url)
+        print(sources_url)
+        if response.status == 'ok':
             for data in response.json()['sources']:
                 sources.append(data)
         print(sources)
@@ -31,19 +32,19 @@ def get_articles(self, article):
         articles = []
         articles_url = 'https://newsapi.org/v2/everything?q={}&apiKey={}'.format(articles, self.api_key)
         response = request.get(articles_url)
-        if response.status_code == 200:
+        if response.status == 'ok':
             for data in response.json()['articles']:
                 articles.append(data)
             print(articles)
 
-            return articles
+        return articles
 
 def get_article_by_source(self, id):
         source_articles = []
         source_articles_url = 'https://newsapi.org/v2/everything?sources={}?apiKey={}'.format(id, self.API_KEY)
         response = request.get(source_articles_url)
-        if response.status_code == 200:
+        if response.status== 'ok':
             for data in response.json()['articles']:
                 source_articles.append(data)
             print(source_articles)
-            return source_articles
+        return source_articles
